7756990995061335a583a52e91a9e4eb76ea118a
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-12-04 10:19:17 +0530

    adding function to roll dice button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import Die from "./die";
+import { useState } from "react";
 
 function App() {
+  const [diceArray, setDiceArray] = useState(generateAllNewDice())
+
+function generateAllNewDice() {
+  return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+}
+
+const diceElement = diceArray.map(dice => <Die value={dice} />)
+
+function diceRoll () {
+   setDiceArray(generateAllNewDice())
+}
+
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceElement}
       </div>
+      <button className="roll-dice" onClick={diceRoll}>Roll</button>
     </main>
   );
 }
